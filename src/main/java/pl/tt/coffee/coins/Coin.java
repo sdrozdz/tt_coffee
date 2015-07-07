39dd3148735f9180578c3e227a005378a9cc2f16
unknown <cyranl@L0437.tt.com.pl>
2015-07-07 11:18:20 +0200

    Added coin classes in coins package

---
+++ src/main/java/pl/tt/coffee/coins/Coin.java
@@ -0,0 +1,12 @@
+package pl.tt.coffee.coins;
+
+/**
+ * Created by cyranl on 2015-07-07.
+ */
+public abstract class Coin {
+    protected double value;
+
+    public double getValue() {
+        return value;
+    }
+}
