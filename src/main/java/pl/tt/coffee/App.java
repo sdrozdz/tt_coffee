28350c826ef5c81af8eb7d73fef90772ca368e4a
unknown <matusiakp@L0485.tt.com.pl>
2015-07-07 11:15:05 +0200

    added logger

--- src/main/java/pl/tt/coffee/App.java
+++ src/main/java/pl/tt/coffee/App.java
@@ -1,13 +1,11 @@
 package pl.tt.coffee;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.apache.log4j.Logger;
+
+public class App {
+	final static Logger logger = Logger.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("test");
+	}
 }
