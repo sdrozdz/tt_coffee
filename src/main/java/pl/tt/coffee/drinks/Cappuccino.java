48d0c3770421dd0a6b7731961c8e58db862eaba6
sdrozdz <seba987xdc@gmail.com>
2015-07-07 10:58:38 +0200

    Added coffee types

---
+++ src/main/java/pl/tt/coffee/drinks/Cappuccino.java
@@ -0,0 +1,8 @@
+package pl.tt.coffee.drinks;
+
+public class Cappuccino extends Coffee {
+    @Override
+    public String getName() {
+        return "Cappuccino";
+    }
+}
---
+++ src/main/java/pl/tt/coffee/drinks/Espresso.java
@@ -0,0 +1,10 @@
+package pl.tt.coffee.drinks;
+
+public class Espresso extends Coffee {
+
+
+    @Override
+    public String getName() {
+        return "Espresso";
+    }
+}
