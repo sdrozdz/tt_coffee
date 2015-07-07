3cd52de5a4da75476942c487cfbe11accf30a253
unknown <cyranl@L0437.tt.com.pl>
2015-07-07 10:56:56 +0200

    Coin classes added

---
+++ src/main/java/pl/tt/coins/FivePLN.java
@@ -0,0 +1,10 @@
+package pl.tt.coins;
+
+/**
+ * Created by cyranl on 2015-07-07.
+ */
+public class FivePLN extends Coin{
+    public FivePLN() {
+        value = 5;
+    }
+}
---
+++ src/main/java/pl/tt/coins/TwoPLN.java
@@ -0,0 +1,10 @@
+package pl.tt.coins;
+
+/**
+ * Created by cyranl on 2015-07-07.
+ */
+public class TwoPLN extends Coin{
+    public TwoPLN() {
+        value = 2;
+    }
+}
